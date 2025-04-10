8f2df86eeeff8abf3386066f81e84a30ee4ab2de
N3agu <andreei1465@yahoo.com>
2025-04-10 12:06:27 +0300

    Refractor XAML code

--- WiredBrainCoffee.CustomerApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomerApp/MainWindow.xaml.cs
@@ -28,9 +28,12 @@ namespace WiredBrainCoffee.CustomerApp
         private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
         {
             //int column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
-            //int newColumn = column == 0 ? 2 : 0; // If it's 0 becomes 2, else 0
+            //int newColumn = column == 0 ? 2 : 0;
             //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+            int column = Grid.GetColumn(customerListGrid);
 
+            int newColumn = column == 0 ? 2 : 0; // If it's 0 becomes 2, else 0
+            Grid.SetColumn(customerListGrid, newColumn);
         }
     }
 }

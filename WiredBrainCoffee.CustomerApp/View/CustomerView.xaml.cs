e5fb5fc81b95f907fca45111b5eb932eb3832d24
N3agu <andreei1465@yahoo.com>
2025-04-10 17:59:32 +0000

    Implemented View Model, modified customers List View

--- WiredBrainCoffee.CustomerApp/View/CustomerView.xaml.cs
+++ WiredBrainCoffee.CustomerApp/View/CustomerView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WiredBrainCoffee.CustomerApp.Data;
+using WiredBrainCoffee.CustomerApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomerApp.View
 {
@@ -20,9 +22,19 @@ namespace WiredBrainCoffee.CustomerApp.View
     /// </summary>
     public partial class CustomerView : UserControl
     {
+        private CustomerViewModel _viewModel;
+
         public CustomerView()
         {
             InitializeComponent();
+            _viewModel = new CustomerViewModel(new CustomerDataProvider());
+            DataContext = _viewModel;
+            Loaded += CustomerView_Loaded;
+        }
+
+        private async void CustomerView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
         }
 
         private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
---
+++ WiredBrainCoffee.CustomerApp/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomerApp.Data;
+using WiredBrainCoffee.CustomerApp.Model;
+
+namespace WiredBrainCoffee.CustomerApp.ViewModel
+{
+    public class CustomerViewModel
+    {
+        private readonly ICustomerDataProvider _customerDataProvider;
+
+        public CustomerViewModel(ICustomerDataProvider customerDataProvider) {
+            _customerDataProvider = customerDataProvider;
+        }
+        public ObservableCollection<Customer> Customers { get; } = new(); // read only property
+    
+        public async Task LoadAsync()
+        {
+            // Check if data isn't loaded yet
+            if (Customers.Any())
+            {
+                return;
+            }
+
+            IEnumerable<Customer>? customers = await _customerDataProvider.GetAllAsync();
+            if(customers is not null)
+            {
+                foreach(Customer? customer in customers)
+                {
+                    Customers.Add(customer);
+                }
+            }
+        }
+    }
+}

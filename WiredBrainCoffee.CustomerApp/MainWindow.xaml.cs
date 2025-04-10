c0e9a35c7ddc6418ad4a434adb8a7cfcd5cd209c
N3agu <andreei1465@yahoo.com>
2025-04-10 11:29:41 +0300

    Add a new column and implemented Move Navigation button

--- WiredBrainCoffee.CustomerApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomerApp/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace WiredBrainCoffee.CustomerApp
         {
             InitializeComponent();
         }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            int column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            int newColumn = column == 0 ? 2 : 0; // If it's 0 becomes 2, else 0
+            customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+        }
     }
 }

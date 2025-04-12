06b0d5bc507636ecbade94d38ad6ac0ab111ef7d
N3agu <andreei1465@yahoo.com>
2025-04-12 07:23:59 +0000

    Set up dependency injection for MainWindow

--- WiredBrainCoffee.CustomerApp/App.xaml.cs
+++ WiredBrainCoffee.CustomerApp/App.xaml.cs
@@ -1,19 +1,35 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
 using WiredBrainCoffee.CustomerApp.Data;
 using WiredBrainCoffee.CustomerApp.ViewModel;
 
-namespace WiredBrainCoffee.CustomerApp
-{
-    public partial class App : Application
-    {
-        protected override void OnStartup(StartupEventArgs e)
-        {
+namespace WiredBrainCoffee.CustomerApp {
+    public partial class App : Application {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App() {
+            ServiceCollection services = new();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        // Contains all the services needed to instantiate the MainWindow
+        private void ConfigureServices(ServiceCollection services) {
+            services.AddTransient<MainWindow>();
+            
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<CustomerViewModel>();
+            services.AddTransient<ProductViewModel>();
+
+            services.AddTransient<ICustomerDataProvider, CustomerDataProvider>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e) {
             base.OnStartup(e);
 
-            MainWindow? mainWindow = new MainWindow(new MainViewModel(
-                new CustomerViewModel(new CustomerDataProvider()),
-                new ProductViewModel()));
-            mainWindow.Show();
+            MainWindow? mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
         }
     }
 }

11b8da792c48a573e0940c73de5978030f827f02
N3agu <andreei1465@yahoo.com>
2025-04-12 11:57:47 +0000

    Created ValidationViewModelBase

---
+++ WiredBrainCoffee.CustomerApp/ViewModel/ValidationViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace WiredBrainCoffee.CustomerApp.ViewModel {
+    internal class ValidationViewModelBase : ViewModelBase, INotifyDataErrorInfo {
+        private readonly Dictionary<string, List<object>> _errorsByPropertyName = new();
+        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return propertyName is not null && _errorsByPropertyName.ContainsKey(propertyName)
+                ? _errorsByPropertyName[propertyName] : Enumerable.Empty<string>();
+        }
+
+        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs args) {
+            ErrorsChanged?.Invoke(this, args);
+        }
+    }
+}
